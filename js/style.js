e2f9b554b789c121d8b80630139d7176d21ddfc8
ILGIZ <ilgiz.zhaparov.on@gmail.com>
2022-01-28 16:08:43 +0800

    js task 6 done

---
+++ js/style.js
@@ -0,0 +1,67 @@
+alert("Я оцениваю свою работу на 90 баллов");
+alert("Спасибо что проверили мою работу!");
+
+const body = document.querySelector("body");
+body.style.background = "hsl(217, 54%, 11%)";
+body.style.fontFamily = "Outfit";
+
+
+const equilib = document.querySelector(".equilib");
+equilib.style.objectFit = "cover";
+equilib.style.width = "100%";
+equilib.style.height = "320px";
+equilib.style.borderRadius = "20px";
+
+
+const wrapper = document.querySelector("#wrapper");
+wrapper.style.margin = "0 auto";
+wrapper.style.height = "620px";
+wrapper.style.width = "350px";
+wrapper.style.background = "hsl(216, 50%, 16%)";
+wrapper.style.padding = "25px";
+wrapper.style.borderRadius = "20px";
+
+
+const title = document.querySelector("#title");
+title.style.fontSize = "25px";
+title.style.color = "hsl(0, 0%, 100%)";
+
+
+const subtitle = document.querySelector(".subtitle");
+subtitle.style.color = "hsl(215, 51%, 70%)";
+subtitle.style.fontSize = "21px";
+
+
+const eth = document.querySelector(".eth");
+eth.style.color = "hsl(178, 100%, 50%)";
+eth.style.marginLeft = "10px";
+
+
+const time = document.querySelector(".time");
+time.style.color = "hsl(215, 51%, 70%)";
+time.style.marginLeft = "10px";
+
+
+const creation = document.querySelector(".creation");
+creation.style.color = "hsl(215, 51%, 70%)";
+creation.style.fontSize = "18px";
+
+const jules = document.querySelector(".jules");
+jules.style.color = "hsl(0, 0%, 100%)";
+jules.style.fontSize = "18px";
+
+const autor = document.querySelector(".autor");
+autor.style.paddingTop = "22px";
+
+const avatar = document.querySelector(".avatar");
+avatar.style.objectFit = "cover";
+avatar.style.height = "35px";
+avatar.style.border = "1px solid white";
+avatar.style.borderRadius = "25px";
+avatar.style.marginRight = "18px";
+avatar.style.marginTop = "15px";
+
+const line = document.querySelector(".line");
+line.style.marginTop = "35px";
+
+
